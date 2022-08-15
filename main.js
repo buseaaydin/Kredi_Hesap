b085badbbaad93f7a2016e0f91d9118ed86d9318
buseaaydin <buseaydin0504@gmail.com>
2022-08-15 19:31:41 +0300

    first commit

--- main.js
+++ main.js
@@ -8,19 +8,19 @@ function krediHesapla()
 
     if(vadeSayisi==6)
     {
-        odenecekTutar=anaPara*1.1;
+        odenecekTutar=anaPara*2.3;
     }
     else if(vadeSayisi==12)
     {
-        odenecekTutar=anaPara*1.2;
+        odenecekTutar=anaPara*3.6;
     }
     else if(vadeSayisi==24)
     {
-        odenecekTutar=anaPara*1.3;
+        odenecekTutar=anaPara*3.8;
     }
     else if(vadeSayisi==30)
     {
-        odenecekTutar=anaPara*1.4;
+        odenecekTutar=anaPara*4.0;
     }
     aylikTaksit=odenecekTutar/vadeSayisi;
 
